50202b4b43be227f82cc6736d239c6abed3c9c3c
MaJesus <mareyes.esp@gmail.com>
2021-07-12 16:14:25 -0400

    no

--- Login/accessLogin.js
+++ Login/accessLogin.js
@@ -6,4 +6,4 @@ signButton.addEventListener('click', () => {
   const email = document.getElementById('email1').value;
   const password = document.getElementById('password1').value;
   classLogin.signIn(email, password);
-});
+});                         
--- router.js
+++ router.js
@@ -1,22 +1,30 @@
 import LoginMethod from './Login/login.js';
 
 const divLogin = document.createElement('div');
+
+const showTemplate = (hash) => {
+  const containerRoot = document.getElementById('root');
+  containerRoot.innerHTML = '';
+
+  switch (hash) {
+    case '#/login':
+      containerRoot.appendChild(login(divLogin));
+      break;
+      case '#/':
+        containerRoot.appendChild(login(divLogin));
+        break;
+      default:
+        containerRoot.appendChild(errorPage());
+    }
+};
+
 export const changeRoute = (hash) => {
   if (hash === '#/login') {
     return showTemplate(hash);
   }
-  
-const showTemplate = (hash) => {
-  const containerRoot = document.getElementById('root');
-  containerRoot.appendChild(LoginMethod.login(divLogin));
-  switch (hash) {
-      case '#/login':
-      containerRoot.appendChild(LoginMethod.login(divLogin));
-      break;
-        case '#':
-        containerRoot.appendChild(LoginMethod.login(divLogin));
-        break;
-    default:
-      break;
+  if (hash === '#/') {
+    return showTemplate(hash);
   }
+  //return '';
+  return showTemplate(hash);
 };
