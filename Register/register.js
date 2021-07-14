a7eeca70636780a94d770df0d163979e78c1dbd1
TeresaOyarce <t.oyarce05@gmail.com>
2021-07-14 10:11:50 -0400

    html comentado, div creados correctamente

--- Register/register.js
+++ Register/register.js
@@ -1,33 +1,34 @@
 export const registerMethod = {
-  register: (emailRegister, passRegister, sucess) => {
-    const sucessCheck = sucess;
-    function check(success) {
-      const succCheck = success;
-      succCheck.innerHTML = '<p>Registro exitoso, verificar email</p>';
-      firebase.auth().currentUser.sendEmailVerification()
-        .then(() => {
-          // Email verification sent!
-          // ...
-        });
-    }
+    register: (emailRegister, passRegister, sucess) => {
+        const sucessCheck = sucess;
 
-    firebase.auth().createUserWithEmailAndPassword(emailRegister, passRegister)
-      .then(() => {
-        check(sucessCheck);
-      })
-      .then((userCredential) => {
-      // Signed in
-        const user = userCredential.user;
-        console.log(user);
-      // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-  },
+        function check(success) {
+            const succCheck = success;
+            succCheck.innerHTML = '<p>Registro exitoso, verificar email</p>';
+            firebase.auth().currentUser.sendEmailVerification()
+                .then(() => {
+                    // Email verification sent!
+                    // ...
+                });
+        }
+
+        firebase.auth().createUserWithEmailAndPassword(emailRegister, passRegister)
+            .then(() => {
+                check(sucessCheck);
+            })
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+                console.log(errorCode);
+                console.log(errorMessage);
+            });
+    },
 };
 export default registerMethod;
