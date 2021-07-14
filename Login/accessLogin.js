c69368b773a6f95ec9d92cce615a5d341aa562ac
MaJesus <mareyes.esp@gmail.com>
2021-07-14 08:26:20 -0400

    ?

--- Login/accessLogin.js
+++ Login/accessLogin.js
@@ -2,8 +2,8 @@ import classLogin from './login.js';
 
 const signButton = document.getElementById('signButton');
 
-signButton.addEventListener('click', () => {
+/*signButton.addEventListener('click', () => {
   const email = document.getElementById('email1').value;
   const password = document.getElementById('password1').value;
   classLogin.signIn(email, password);
-});                         
+});                         */
--- Register/accessRegister.js
+++ Register/accessRegister.js
@@ -2,9 +2,9 @@ import classRegister from './register.js';
 
 const buttonR = document.getElementById('buttonRegister');
 
-buttonR.addEventListener('click', () => {
+/*buttonR.addEventListener('click', () => {
   const email = document.getElementById('email2').value;
   const password = document.getElementById('password2').value;
   const success = document.getElementById('success');
   classRegister.register(email, password, success);
-});
+});*/
