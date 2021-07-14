bb34402fcf622b61b078ea9188377e11c2723542
MaJesus <mareyes.esp@gmail.com>
2021-07-14 19:56:08 -0400

    Divs

--- src/Login/login.js
+++ src/Login/login.js
@@ -6,43 +6,86 @@ const divRoot = document.getElementById('root');
 
 const LoginMethod = {
 
-    login: (divLogin) => {
-        const contDivLogin = document.createElement('div');
+  login: (divLogin) => {
+    const contDivLogin = document.createElement('div');
 
-        // Input Email
-        contDivLogin.appendChild(createElement('input', 'userInput', 'userInput', 'email', 'Ingresa tu correo', ''));
+    //DIV IZQUIERDO
+    const leftDiv = document.createElement('div');
+    leftDiv.classList.add('leftDiv');
 
-        // Input password
-        contDivLogin.appendChild(createElement('input', 'passInput', 'passInput', 'password', 'Contraseña', ''));
+    //imagen
+    leftDiv.appendChild(createElement('img', '', 'imgLeft', '', '', './img/houseac.png'));
+
+    contDivLogin.appendChild(leftDiv);
+
+    //DIV DERECHO
+    const rightDiv = document.createElement('div');
+    rightDiv.classList.add('rightDiv');
+
+    //Head Div
+    const headDiv = createElement('div');
+    headDiv.classList.add('headDiv');
+
+    //logo
+    headDiv.appendChild(createElement('img', '', 'logo', '', '', './img/logo.png'))
+
+    //nook-Nook
+    const logo = document.createElement('p');
+    logo.classList.add('logo');
+    logo.textContent = 'Nook-Nook';
+    headDiv.appendChild(logo);
+
+    rightDiv.appendChild(headDiv);
+
+    //Mid Div
+    const midDiv = createElement('div');
+    midDiv.classList.add('midDiv');
+
+    // input Email
+    midDiv.appendChild(createElement('input', 'userInput', 'userInput', 'email', 'Ingresa tu correo', ''));
+
+    // input password
+    midDiv.appendChild(createElement('input', 'passInput', 'passInput', 'password', 'Contraseña', ''));
 
 
-        //Boton Login
-        const loginButton = createElement('button', 'loginButton', 'loginButton', '', 'Iniciar Sesión', '');
+    //boton Login
+    const loginButton = createElement('button', 'loginButton', 'loginButton', '', 'Iniciar Sesión', '');
 
-        contDivLogin.appendChild(loginButton);
+    midDiv.appendChild(loginButton);
 
-        loginButton.addEventListener('click', () => {
-            const email = userInput.value;
-            const password = passInput.value;
+    loginButton.addEventListener('click', () => {
+      const email = userInput.value;
+      const password = passInput.value;
+      normalAccess(email, password);
+    });
 
-            normalAccess(email, password);
-        });
+    //login Google
+    const googleLogin = createElement('button', 'googleLogin', 'googleLogin', '', 'Iniciar Sesión con Google', '');
 
-        //Login Google
-        const googleLogin = createElement('button', 'googleLogin', 'googleLogin', '', 'Iniciar Sesión con Google', '');
-        contDivLogin.appendChild(googleLogin);
-        googleLogin.addEventListener('click', () => {
-            googleAccess();
-        });
+    midDiv.appendChild(googleLogin);
 
+    googleLogin.addEventListener('click', () => {
+      googleAccess();
+    });
 
-        divRoot.appendChild(contDivLogin);
+    contDivLogin.appendChild(midDiv);
 
+    //Foot Div
+    const footDiv = createElement('footer');
 
+    var element = document.createElement("a");
+    element.setAttribute("href", 'www.google.cl');
+    element.innerHTML = "REGISTRATE AQUÍ";
 
+    footDiv.appendChild(element);
 
-        return divLogin;
-    },
+    rightDiv.appendChild(footDiv);
+
+    contDivLogin.appendChild(rightDiv);
+
+    divRoot.appendChild(contDivLogin);
+    return divLogin;
+  },
 };
 
 
