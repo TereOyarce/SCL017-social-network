ad0a630b5460a3c01859b801164442a91a21b6ba
MaJesus <mareyes.esp@gmail.com>
2021-07-17 19:58:16 -0400

    Botoncito Registro, aún falta lo de las rutas

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function observer() {
             console.log(user.displayName);
             // User is signed in, see docs for a list of available properties
             // https://firebase.google.com/docs/reference/js/firebase.User
-            //show(user);
+            show(user);
             let emailVerified = user.emailVerified;
             const uid = user.uid;
             console.log(emailVerified);
