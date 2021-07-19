26c228c15a51cc4896f4a727166493700bc6a10f
MaJesus <mareyes.esp@gmail.com>
2021-07-19 10:21:45 -0400

    Sin #/

--- src/Wall/wall.js
+++ src/Wall/wall.js
@@ -2,13 +2,13 @@ const root = document.getElementById('root');
 
 const wall = {
     feed: (divFeed) => {
-        console.log('este es el muro');
+        
         const div = document.createElement('div');
         const prueba = document.createElement('p');
         prueba.textContent = 'holaa';
         div.appendChild(prueba);
         root.appendChild(div);
-
+        console.log('Dos veces?');
         return divFeed;
     }
 
