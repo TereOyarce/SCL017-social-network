685c65e3c5d0061c337d2351ed065bab5476ce65
MaJesus <mareyes.esp@gmail.com>
2021-07-19 21:05:59 -0400

    CRUD

--- Wall/wall.js
+++ Wall/wall.js
@@ -1,6 +1,10 @@
 import { firebaseLogout } from '../main.js';
 import { createElement } from '../method/MethodCreateElement.js';
 import { createDiv } from '../method/divCreator.js';
+import { database } from './accessWall.js';
+import { savePost } from './accessWall.js';
+//import { getPost } from './accessWall.js';
+
 
 const root = document.getElementById('root');
 
@@ -11,7 +15,7 @@ const wall = {
 
         //Navigation Menu
         const navContainer = createDiv('div', 'navContainer', 'navContainer');
-        
+
         //Logout button
         const logoutButton = createElement('button', 'logoutButton', 'logoutButton', '', 'Cerrar Sesión', '');
         logoutButton.addEventListener('click', () => {
@@ -22,17 +26,31 @@ const wall = {
         //Body post
         const bodyContainer = createDiv('div', 'bodyContainer', 'bodyContainer');
 
+        const form = document.createElement('form');
+        form.id = 'formId';
+
         const inputPost = createElement('input', 'inputPost', 'inputPost', 'text', '¿Qué estás pensando?', '');
         const postButton = createElement('button', 'postButton', 'postButton', '', 'Publicar', '');
 
+        
+        
+
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const description = form['inputPost'].value;
+            await savePost(description);
+            form.reset();
+            console.log(description);
+        })
 
 
 
 
+        form.appendChild(inputPost);
+        form.appendChild(postButton);
 
         navContainer.appendChild(logoutButton);
-        bodyContainer.appendChild(inputPost);
-        bodyContainer.appendChild(postButton);
+        bodyContainer.appendChild(form);
 
         wallContainer.appendChild(navContainer);
         wallContainer.appendChild(bodyContainer);
@@ -41,13 +59,6 @@ const wall = {
 
 
         root.appendChild(wallContainer);
-
-
-
-
-
-
-
 
 
         return divFeed;
